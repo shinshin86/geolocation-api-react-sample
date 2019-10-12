26396940c9e98ab58f3bf3d4e528136b356f6a22
shinshin86 <beagles1986@gmail.com>
2019-10-12 19:02:56 +0900

    Add watch mode

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ const ErrorText = () => (
 export default () => {
   const [isAvailable, setAvailable] = useState(false);
   const [position, setPosition] = useState({ latitude: null, longitude: null });
+  const [watchStatus, setWatchStatus] = useState({
+    isWatching: false,
+    watchId: null
+  });
 
   // Used to determine if "useEffect" is already used
   const isFirstRef = useRef(true);
@@ -26,20 +30,52 @@ export default () => {
     });
   };
 
+  const startWatchPosition = () => {
+    const watchId = navigator.geolocation.watchPosition(position => {
+      const { latitude, longitude } = position.coords;
+      setPosition({ latitude, longitude });
+    });
+
+    setWatchStatus({ isWatching: true, watchId });
+  };
+
+  const stopWatchPosition = watchStatus => {
+    navigator.geolocation.clearWatch(watchId);
+    setWatchStatus({ isWatching: false, watchId });
+  };
+
   // Show this(Loading...) until "useEffect" is completed
   if (isFirstRef.current) return <div className="App">Loading...</div>;
 
+  const { isWatching, watchId } = watchStatus;
+
   return (
     <div className="App">
-      <p>Geolocation API Sample</p>
+      <h2>Geolocation API Sample</h2>
       {!isFirstRef && !isAvailable && <ErrorText />}
       {isAvailable && (
         <div>
           <button onClick={getCurrentPosition}>Get Current Position</button>
+          {!isWatching && (
+            <button onClick={startWatchPosition}>Start Watch Position</button>
+          )}
+          {isWatching && (
+            <button onClick={() => stopWatchPosition(watchStatus)}>
+              Stop Watch Position
+            </button>
+          )}
           <div>
-            latitude: {position.latitude}
-            <br />
-            longitude: {position.longitude}
+            <h3>Position</h3>
+            <div>
+              latitude: {position.latitude}
+              <br />
+              longitude: {position.longitude}
+            </div>
+          </div>
+          <div>
+            <h3>Watch Mode</h3>
+            <p>Watch Status: {isWatching ? 'Watching' : 'Not Watching'}</p>
+            <p>Watch ID: {watchId}</p>
           </div>
         </div>
       )}
