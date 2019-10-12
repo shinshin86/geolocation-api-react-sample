58612e268d04d98914b13a530c685694296a5196
shinshin86 <beagles1986@gmail.com>
2019-10-12 18:05:03 +0900

    Create Geolocation API Sample

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
-function App() {
+const ErrorText = () => (
+  <p className="App-error-text">geolocation IS NOT available</p>
+);
+
+export default () => {
+  const [isAvailable, setAvailable] = useState(false);
+  const [position, setPosition] = useState({ latitude: null, longitude: null });
+
+  // Used to determine if "useEffect" is already used
+  const isFirstRef = useRef(true);
+
+  useEffect(() => {
+    isFirstRef.current = false;
+    if ('geolocation' in navigator) {
+      setAvailable(true);
+    }
+  }, [isAvailable]);
+
+  const getCurrentPosition = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      setPosition({ latitude, longitude });
+    });
+  };
+
+  // Show this(Loading...) until "useEffect" is completed
+  if (isFirstRef.current) return <div className="App">Loading...</div>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Geolocation API Sample</p>
+      {!isFirstRef && !isAvailable && <ErrorText />}
+      {isAvailable && (
+        <div>
+          <button onClick={getCurrentPosition}>Get Current Position</button>
+          <div>
+            latitude: {position.latitude}
+            <br />
+            longitude: {position.longitude}
+          </div>
+        </div>
+      )}
     </div>
   );
-}
-
-export default App;
+};
